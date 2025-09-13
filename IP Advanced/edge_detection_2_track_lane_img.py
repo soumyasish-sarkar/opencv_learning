66f9d13511f0ba11c9ab38e4d859dd87b996942f
soumyasish-sarkar <ssoumyasish@gmail.com>
2025-09-13 22:38:40 +0530

    image -> convert ot greyscale -> gaussianBlur (updated from half height to 1.7 times of height to reduce area of computation and reduce lane intersect problem ) -> Adaptive threshold for canny edge detection -> Canny -> ROI -> Hough transformation -> Hough line -> Lane Drawn on original image ->  ; Problem : perfect for straight road but not for turn and road shifted in one side :

--- IP Advanced/edge_detection_2_track_lane_img.py
+++ IP Advanced/edge_detection_2_track_lane_img.py
@@ -1,73 +1,90 @@
 import cv2
 import numpy as np
 
-img = cv2.imread(r"C:\Users\soumyasish-sarkar\PycharmProjects\opencv_learning\image\road1.jpg")
-img=cv2.resize(img,(640,360))
+def lane_detection_IP(frame):
+    #frame = cv2.resize(frame, (640, 360))
 
-#image -> gaussianBlur -> Canny -> ROI
+    # image -> gaussianBlur -> Canny -> ROI
 
-#Applying GaussianBlur
-img_g = cv2.GaussianBlur(img, (5,5), 0)
-#Canny Edge detection
-canny=cv2.Canny(img_g,50,150)
+    # converting image to greyscale
+    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#ROI - Region of Interest
-#Gets the size of the input image (grayscale edge image), height = number of rows (y-axis), width = number of columns (x-axis)
-height, width = canny.shape
-#Creates a black mask (all zeros) with the same size as the image, will be used to "cut out" the region you want to keep.
-mask = np.zeros_like(canny)
-# ROI polygon Area
-polygon = np.array([[
-        (0, height),      # bottom-left
+    # Applying GaussianBlur
+    frame_gaussian = cv2.GaussianBlur(frame_grey, (5, 5), 0)
+
+    # Adaptive canny thresholding
+    # compute median pixel value
+    median_pix = np.median(frame_gaussian)
+
+    # set threashold based on median
+    lower = int(max(0, 0.7 * median_pix))
+    upper = int(min(255, 1.3 * median_pix))
+
+    # Canny Edge detection  ##For adaptive threshold we won't use normal canny
+    frame_canny = cv2.Canny(frame_gaussian, lower, upper)
+
+
+    # ROI - Region of Interest
+    # Gets the size of the input image (grayscale edge image), height = number of rows (y-axis), width = number of columns (x-axis)
+    height, width = frame_canny.shape
+    # Creates a black mask (all zeros) with the same size as the image, will be used to "cut out" the region you want to keep.
+    mask = np.zeros_like(frame_canny)
+    # ROI polygon Area
+    polygon = np.array([[
+        (0, height),  # bottom-left
         (width, height),  # bottom-right
-        #((3*width )// 4, height // 2),  # middle-right
-        (width,height // 2 ),
-        #(width // 4, height // 2)  # middle-left
-        (0, height // 2 ),
+        # ((3*width )// 4, height // 2),  # middle-right
+        (width, height // 1.7),
+        # (width // 4, height // 2)  # middle-left
+        (0, height // 1.7),
     ]], np.int32)
 
-#Fill ROI with white
-cv2.fillPoly(mask, polygon, 255)
-#Resultanted ROI
-roi = cv2.bitwise_and(canny, mask)
+    # Fill ROI with white
+    cv2.fillPoly(mask, polygon, 255)
+    # Resultanted ROI
+    frame_roi = cv2.bitwise_and(frame_canny, mask)
 
-#Hough Line Transformation to detect the straight line path
-houghLine = cv2.HoughLinesP(
-        roi,
-        rho=1, #journal rho = 1
-        theta=np.pi/180,
-        threshold=150, #Journal threshold = 100 #at 50 noisy line getting marked
-        minLineLength=100, #Journal minLineLength=100
-        maxLineGap=50 #Journal maxLineGap=50
+    # Hough Line Transformation to detect the straight line path
+    houghLine = cv2.HoughLinesP(
+        frame_roi,
+        rho=1,  # journal rho = 1
+        theta=np.pi / 180,
+        threshold=150,  # Journal threshold = 100 #at 50 noisy line getting marked
+        minLineLength=100,  # Journal minLineLength=100
+        maxLineGap=50  # Journal maxLineGap=50
     )
 
-#copy of canny
-hough = np.copy(img)
+    # copy of canny
+    frame_hough = np.copy(frame)
 
-#drawing hough lines on the image
-# if houghLine is not None:
-#     for line in houghLine:
-#         x1, y1, x2, y2 = line[0]
-#         cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 2)
-#
-if houghLine is not None:
-    for line in houghLine:
-        x1, y1, x2, y2 = line[0]
+    # drawing hough lines on the image
+    # if houghLine is not None:
+    #     for line in houghLine:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #
+    if houghLine is not None:
+        for line in houghLine:
+            x1, y1, x2, y2 = line[0]
 
-        # Calculate angle in degrees
-        angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
+            # Calculate angle in degrees
+            angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
 
-        # Filter: ignore near-horizontal lines
-        if abs(angle) <5:  # threshold angle, adjust if needed
-            continue
-        cv2.line(hough, (x1, y1), (x2, y2), (0, 255, 0), 3)  # green lines
+            # Filter: ignore near-horizontal lines
+            if abs(angle) < 5:  # threshold angle, adjust if needed
+                continue
+            cv2.line(frame_hough, (x1, y1), (x2, y2), (0, 255, 0), 3)  # green lines
 
-#displaying the images
-cv2.imshow("image",img)
-#cv2.imshow("Canny ED - with Gaussian Blur",canny)
-cv2.imshow("ROI",roi)
-cv2.imshow("Hough Line",hough)
+    return frame_hough
 
 
+
+
+img = cv2.imread(r"C:\Users\soumyasish-sarkar\PycharmProjects\opencv_learning\image\road6.jpg")
+img = cv2.resize(img, (640, 360))
+frame = lane_detection_IP(img)
+
+cv2.imshow("Original image", img)
+cv2.imshow("Lane detected image", frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
